7719990c59df523f32d0378ea637d8aa38483c8e
Sean Parent <sean.parent@stlab.cc>
2025-07-18 08:07:19 -0700

    Grammar fixes (#1)
    Matching (subset) of Rust grammar (except literals and identifiers, which are not a proper subset).

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,19 @@ use untwine::{parser, parser_repl};
 
 parser! {
     [recover = true]
-    sep = #{char::is_ascii_whitespace}*;
+    __ = #{char::is_ascii_whitespace}*;
     pub expression = or_expression -> ();
-    or_expression = (and_expression sep "||" sep and_expression | and_expression ) -> ();
-    and_expression = (comparison_expression sep "&&" sep comparison_expression | comparison_expression ) -> ();
-    comparison_expression = (bitwise_or_expression sep ("==" | "!=" | "<" | ">" | "<=" | ">=") sep bitwise_or_expression | bitwise_or_expression ) -> ();
-    bitwise_or_expression = (bitwise_xor_expression sep "|" sep bitwise_xor_expression | bitwise_xor_expression) -> ();
-    bitwise_xor_expression = (bitwise_and_expression sep "^" sep bitwise_and_expression | bitwise_and_expression) -> ();
-    bitwise_and_expression = (bitwise_shift_expression sep "&" sep bitwise_shift_expression | bitwise_shift_expression) -> ();
-    bitwise_shift_expression = (additive_expression sep ("<<" | ">>") sep additive_expression | additive_expression) -> ();
-    additive_expression = (multiplicative_expression sep ("+" | "-") sep multiplicative_expression | multiplicative_expression) -> ();
-    multiplicative_expression = (unary_expression sep ("*" | "/" | "%") sep unary_expression | unary_expression) -> ();
-    unary_expression = ((("-" | "!") unary_expression) | primary_expression) -> ();
-    primary_expression = ("(" sep expression sep ")" | literal | identifier) -> ();
+    or_expression = and_expression (__ "||" __ and_expression)* -> ();
+    and_expression = comparison_expression (__ "&&" __ comparison_expression)* -> ();
+    comparison_expression = bitwise_or_expression (__ ("==" | "!=" | "<" | ">" | "<=" | ">=") __ bitwise_or_expression)? -> ();
+    bitwise_or_expression = bitwise_xor_expression (__ "|" __ bitwise_xor_expression)* -> ();
+    bitwise_xor_expression = bitwise_and_expression (__ "^" __ bitwise_and_expression)* -> ();
+    bitwise_and_expression = bitwise_shift_expression (__ "&" __ bitwise_shift_expression)* -> ();
+    bitwise_shift_expression = additive_expression (__ ("<<" | ">>") __ additive_expression)* -> ();
+    additive_expression = multiplicative_expression (__ ("+" | "-") __ multiplicative_expression)* -> ();
+    multiplicative_expression = unary_expression (__ ("*" | "/" | "%") __ unary_expression)* -> ();
+    unary_expression = ((("-" | "!") __ unary_expression) | primary_expression) -> ();
+    primary_expression = ("(" __ expression __ ")" | literal | identifier) -> ();
     literal = #["0123456789"]+ -> ();
     identifier = #["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz_"]+ -> ();
 }
